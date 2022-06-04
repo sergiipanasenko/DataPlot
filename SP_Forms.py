ea24f818a4e94ecccb6d3629e4f32e2594d56282
Sergii <sergii.v.panasenko@gmail.com>
2022-06-04 20:53:40 +0300

    Improving the code. Adding some methods.

--- SP_Forms.py
+++ SP_Forms.py
@@ -38,7 +38,7 @@ class MyTableSubWindow(QtWidgets.QWidget, MyAbstractForm):
 class MyTabSubWindow(QtWidgets.QWidget, MyAbstractForm):
     def __init__(self):
         # parent initialisation
-        QtWidgets.QWidget.__init__(self)
+        super().__init__(self)
 
         # UI loading
         uic.loadUi('UI/MyTabWidget.ui', self)
@@ -109,11 +109,16 @@ class MyForm3(QtWidgets.QMainWindow, MyAbstractForm):
         self.combo_Ncolto = self.findChild(QtWidgets.QComboBox, "combo_Ncolto")
         self.combo_Nrowfrom = self.findChild(QtWidgets.QComboBox, "combo_Nrowfrom")
         self.combo_Nrowto = self.findChild(QtWidgets.QComboBox, "combo_Nrowto")
-        self.combo_Datacolfrom = self.findChild(QtWidgets.QComboBox, "combo_Xcolfrom")
-        self.combo_Datacolto = self.findChild(QtWidgets.QComboBox, "combo_Xcolto")
-        self.combo_Datarowfrom = self.findChild(QtWidgets.QComboBox, "combo_Xrowfrom")
-        self.combo_Datarowto = self.findChild(QtWidgets.QComboBox, "combo_Xrowto")
+        self.combo_Datacolfrom = self.findChild(QtWidgets.QComboBox, "combo_Datacolfrom")
+        self.combo_Datacolto = self.findChild(QtWidgets.QComboBox, "combo_Datacolto")
+        self.combo_Datarowfrom = self.findChild(QtWidgets.QComboBox, "combo_Datarowfrom")
+        self.combo_Datarowto = self.findChild(QtWidgets.QComboBox, "combo_Datarowto")
         self.combo_wise = self.findChild(QtWidgets.QComboBox, "combo_wise")
+
+        self.label_totalrowvalue = self.findChild(QtWidgets.QLabel, "label_totalrowvalue")
+        self.label_totalcolumnvalue = self.findChild(QtWidgets.QLabel, "label_totalcolumnvalue")
+        self.label_currentrowvalue = self.findChild(QtWidgets.QLabel, "label_currentrowvalue")
+        self.label_currentcolumnvalue = self.findChild(QtWidgets.QLabel, "label_currentcolumnvalue")
 
         self.mdiArea = self.findChild(QtWidgets.QMdiArea, "mdiArea")
 
@@ -169,19 +174,12 @@ class MyForm3(QtWidgets.QMainWindow, MyAbstractForm):
         self.actionTiled.triggered.connect(self.mdiArea.tileSubWindows)
         self.actionCascaded.triggered.connect(self.mdiArea.cascadeSubWindows)
 
-        self.actionRow_above.triggered \
-            .connect(lambda: self.current_table.insertRow(self.current_table.currentRow()))
-        self.actionRow_below.triggered \
-            .connect(lambda: self.current_table.insertRow(self.current_table.currentRow() + 1))
-        self.actionColumn_left.triggered \
-            .connect(lambda: self.current_table.insertColumn(self.current_table.currentColumn()))
-        self.actionColumn_right.triggered \
-            .connect(lambda: self.current_table.insertColumn(self.current_table.currentColumn() + 1))
-        self.actionRow.triggered \
-            .connect(lambda: self.current_table.removeRow(self.current_table.currentRow()))
-        self.actionColumn.triggered \
-            .connect(lambda: self.current_table.removeColumn(self.current_table.currentColumn()))
-
+        self.actionRow_above.triggered.connect(lambda: self._add_row(True))
+        self.actionRow_below.triggered.connect(lambda: self._add_row(False))
+        self.actionColumn_left.triggered.connect(lambda: self._add_column(True))
+        self.actionColumn_right.triggered.connect(lambda: self._add_column(False))
+        self.actionRow.triggered.connect(self._remove_row)
+        self.actionColumn.triggered.connect(self._remove_column)
         self.push_cancel.clicked.connect(QtWidgets.qApp.quit)
 
         # self.combo_Xrowcol.currentIndexChanged.connect(self.XRowColChange)
@@ -206,7 +204,52 @@ class MyForm3(QtWidgets.QMainWindow, MyAbstractForm):
         # self.combo_Datacolend.currentIndexChanged.connect(self.DataChangeState)
         self.mdiArea.subWindowActivated.connect(self.sub_window_change)
 
-    def create_new_file(self):
+    def _change_table(self):
+        self.combo_Xrowfrom.clear()
+        self.combo_Xrowto.clear()
+        self.combo_Xcolfrom.clear()
+        self.combo_Xcolto.clear()
+        self.combo_Yrowfrom.clear()
+        self.combo_Yrowto.clear()
+        self.combo_Ycolfrom.clear()
+        self.combo_Ycolto.clear()
+        self.combo_Datarowfrom.clear()
+        self.combo_Datarowto.clear()
+        self.combo_Datacolfrom.clear()
+        self.combo_Datacolto.clear()
+        if self.current_table.columnCount() and self.current_table.rowCount():
+            row_number_list = list(map(str, range(1, self.current_table.rowCount() + 1)))
+            col_number_list = list(map(str, range(1, self.current_table.columnCount() + 1)))
+            self.combo_Xrowfrom.addItems(row_number_list)
+            self.combo_Xrowfrom.setCurrentIndex(0)
+            self.combo_Xrowto.addItems(row_number_list)
+            self.combo_Xrowto.setCurrentIndex(len(row_number_list) - 1)
+            self.combo_Xcolfrom.addItems(col_number_list)
+            self.combo_Xcolfrom.setCurrentIndex(0)
+            self.combo_Xcolto.addItems(col_number_list)
+            self.combo_Xcolto.setCurrentIndex(len(col_number_list) - 1)
+            self.combo_Yrowfrom.addItems(row_number_list)
+            self.combo_Yrowfrom.setCurrentIndex(0)
+            self.combo_Yrowto.addItems(row_number_list)
+            self.combo_Yrowto.setCurrentIndex(len(row_number_list) - 1)
+            self.combo_Ycolfrom.addItems(col_number_list)
+            self.combo_Ycolfrom.setCurrentIndex(0)
+            self.combo_Ycolto.addItems(col_number_list)
+            self.combo_Ycolto.setCurrentIndex(len(col_number_list) - 1)
+            self.combo_Datarowfrom.addItems(row_number_list)
+            self.combo_Datarowfrom.setCurrentIndex(0)
+            self.combo_Datarowto.addItems(row_number_list)
+            self.combo_Datarowto.setCurrentIndex(len(row_number_list) - 1)
+            self.combo_Datacolfrom.addItems(col_number_list)
+            self.combo_Datacolfrom.setCurrentIndex(0)
+            self.combo_Datacolto.addItems(col_number_list)
+            self.combo_Datacolto.setCurrentIndex(len(col_number_list) - 1)
+        self.label_totalrowvalue.setText(str(self.current_table.rowCount()))
+        self.label_totalcolumnvalue.setText(str(self.current_table.columnCount()))
+        self.label_currentrowvalue.setText(str(self.current_table.currentRow() + 1))
+        self.label_currentcolumnvalue.setText(str(self.current_table.currentColumn() + 1))
+
+    def _create_sub_window(self):
         if not self.menuAdd.isEnabled():
             self.menuAdd.setEnabled(True)
             self.actionRow_above.setEnabled(True)
@@ -216,17 +259,48 @@ class MyForm3(QtWidgets.QMainWindow, MyAbstractForm):
             self.menuRemove.setEnabled(True)
             self.actionRow.setEnabled(True)
             self.actionColumn.setEnabled(True)
-        self.statusbar.showMessage('Creating new data file...')
+
+    def _create_table_sub_window(self, sub_window_title, sub_window_icon):
+        self._create_sub_window()
         new_sub_window = MyTableSubWindow()
         self.sub_window = new_sub_window
         self.sub_window_number += 1
         self.sub_window_amount += 1
         self.sub_window.setObjectName('sub-window ' + str(self.sub_window_number))
-        self.sub_window.setWindowTitle('Data ' + str(self.sub_window_number))
-        self.sub_window.setWindowIcon(QtGui.QIcon("UI/New_Icons/add-file.png"))
+        self.sub_window.setWindowTitle(sub_window_title)
+        self.sub_window.setWindowIcon(QtGui.QIcon(sub_window_icon))
         self.current_table = self.sub_window.table
         self.mdiArea.addSubWindow(self.sub_window)
         self.sub_window.show()
+        self._change_table()
+
+    def _add_row(self, flag):
+        if flag:
+            self.current_table.insertRow(self.current_table.currentRow())
+        else:
+            self.current_table.insertRow(self.current_table.currentRow() + 1)
+        self._change_table()
+
+    def _add_column(self, flag):
+        if flag:
+            self.current_table.insertColumn(self.current_table.currentColumn())
+        else:
+            self.current_table.insertColumn(self.current_table.currentColumn() + 1)
+        self._change_table()
+
+    def _remove_row(self):
+        self.current_table.removeRow(self.current_table.currentRow())
+        self._change_table()
+
+    def _remove_column(self):
+        self.current_table.removeColumn(self.current_table.currentColumn())
+        self._change_table()
+
+    def create_new_file(self):
+        self.statusbar.showMessage('Creating new data file...')
+        title = 'Data ' + str(self.sub_window_number + 1)
+        icon = 'UI/New_Icons/add-file.png'
+        self._create_table_sub_window(title, icon)
 
     def open_data_file(self):
         pass
@@ -238,3 +312,5 @@ class MyForm3(QtWidgets.QMainWindow, MyAbstractForm):
         self.sub_window = self.mdiArea.activeSubWindow()
         table = self.sub_window.widget().findChild(QtWidgets.QTableWidget, 'table')
         self.current_table = table
+        self._change_table()
+
