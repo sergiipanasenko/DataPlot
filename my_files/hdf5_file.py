beb0d5770301ec670c24a58bf4b766341fc14616
Sergii <sergii.v.panasenko@gmail.com>
2022-09-26 20:57:27 +0300

    Writing read_h5_data and parse_h5_data functions.

--- my_files/hdf5_file.py
+++ my_files/hdf5_file.py
@@ -8,10 +8,24 @@ class MyHDF5File(MyFile):
         MyFile.__init__(self, file_name)
         self.data = dict()
 
+    @staticmethod
+    def parse_h5_data(data):
+        if isinstance(data, h5.Dataset):
+            output = []
+            if data.dtype.fields.keys():
+                output.append(list(data.dtype.fields.keys()))
+            for row in data:
+                output.append(list(row))
+            return output
+        elif isinstance(data, h5.File) or isinstance(data, h5.Group):
+            output = dict()
+            data = dict(data)
+            for key in data.keys():
+                output[key] = MyHDF5File.parse_h5_data(data[key])
+            return output
+
     def read_h5_data(self, file_name=None):
         if (self.file_name != file_name) or (self.mod_time != getmtime(file_name)):
             self._change_file_name(file_name)
             with h5.File(self.file_name, 'r') as h5_file:
-                data = dict(h5_file)
-                data1 = data['Metadata']
-                print("Keys: %s" % h5_file.keys())
+                self.data = self.parse_h5_data(h5_file)
