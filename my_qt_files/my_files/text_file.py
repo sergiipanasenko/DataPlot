885a3a2e047f41d26dc9fe54e5dd22188b43ac8a
Sergii <sergii.v.panasenko@gmail.com>
2023-04-13 16:14:13 +0300

    Refactoring of text_file.py

--- my_qt_files/my_files/text_file.py
+++ my_qt_files/my_files/text_file.py
@@ -6,39 +6,40 @@ class MyTextFile(MyFile, IText):
         super().__init__(file_name)
 
     def read_text(self, file_name=None):
-        if self.check_file_name(file_name) is not None:
-            with open(file_name) as file:
-                text = file.read()
-            self.set_text(text)
+        file_name = self.check_file_name(file_name)
+        with open(file_name) as file:
+            text = file.read()
+        self.set_text(text)
 
     def write_text(self, text: str, marker='a', file_name=None):
-        if self.check_file_name(file_name) is not None:
-            if marker != 'a' and marker != 'w':
-                marker = 'a'
-            with open(file_name, marker) as file:
-                file.write(text)
+        file_name = self.check_file_name(file_name)
+        if marker != 'a' and marker != 'w':
+            marker = 'a'
+        with open(file_name, marker) as file:
+            file.write(text)
 
 
-class MyTextDataFile(MyTextFile, IData):
+class MyTextDataFile(MyFile, IData):
     def __init__(self, file_name=None):
         super().__init__(file_name)
 
     def read_data(self, file_name=None):
-        if self.check_file_name(file_name) is not None:
-            self.read_text(file_name)
-            text = self.get_text().strip()
-            data = []
-            for row in text.splitlines():
-                row_data = row.rstrip().split()
-                data.append(row_data)
-            self.set_data({'data': data})
+        file_name = self.check_file_name(file_name)
+        text_file = MyTextFile(file_name)
+        text_file.read_text()
+        text = text_file.get_text().strip()
+        data = []
+        for row in text.splitlines():
+            row_data = row.rstrip().split()
+            data.append(row_data)
+        self.set_data({'data': data})
 
     def write_data(self, data: list, marker='a', file_name=None):
-        if self.check_file_name(file_name) is not None:
-            if marker != 'a' and marker != 'w':
-                marker = 'a'
-            with open(file_name, marker) as file:
-                for row in data:
-                    for item in row:
-                        file.write(item)
-                    file.write('\n')
+        file_name = self.check_file_name(file_name)
+        if marker != 'a' and marker != 'w':
+            marker = 'a'
+        with open(file_name, marker) as file:
+            for row in data:
+                for item in row:
+                    file.write(item)
+                file.write('\n')
